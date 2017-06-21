94114bd6eb7fe66a2513a326d60eb12b7ffdffb9
NACHURY MATHIEU p1615457 <mathieuNachury@outlook.com>
2017-06-21 11:11:41 +0200

    commit de ratrapage, timeline + debut de tweet

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Timeline">
+    <h1>Bienvenue sur votre timeline</h1>
+    <ul>
+      <li v-for="tweet in tweets">
+        {{ tweet.auteur + " : " + tweet.message }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tweets',
+  data () {
+    return {
+      tweets: [{auteur: 'drumpf', message: 'covfefe'}, {auteur: 'petit', message: '2'}, {auteur: 'petit', message: '->'}]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+</style>
---
+++ frontend/src/components/Tweet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Tweet">
+    {{ tweet }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tweet',
+  props : [tweet: {auteur: 'drumpf', message: 'covfefe'}]
+}
+</script>
+
+<style>
+</style>
